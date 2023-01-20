cb33cdc12fc78a8847cd90f8eaa33d706ecbc9ba
aaj4001 <aaj4001@med.cornell.edu>
2023-01-19 22:04:36 -0500

    Added Kinetic Set Data File and Derivation Script

---
+++ DerivingKineticSets.R
@@ -0,0 +1,91 @@
+library(GEOquery)
+library(readr)
+library(mogene10sttranscriptcluster.db)
+library(mogene20sttranscriptcluster.db)
+library(mouse4302.db)
+
+## Imports Datasets #########################################################################################################
+## Wherry
+Wherry = getGEO("GSE41867")
+Wherry_Exprs = data.frame(exprs(Wherry[[1]]))
+
+Annotated = data.frame(ACCNUM = sapply(contents(mogene10sttranscriptclusterACCNUM), paste, collapse = ","),SYMBOL = sapply(contents(mogene10sttranscriptclusterSYMBOL),paste,collapse = ","))
+Wherry_Exprs <- merge(Annotated, Wherry_Exprs,by=0,all=T)
+
+Wherry_Exprs_Matrix = data.matrix(Wherry_Exprs[,4:ncol(Wherry_Exprs)])
+rownames(Wherry_Exprs_Matrix) = toupper(Wherry_Exprs$SYMBOL)
+colnames(Wherry_Exprs_Matrix) = c("N_1","N_2","N_3","N_4","Arm_D6_1","Arm_D6_2","Arm_D6_3","Arm_D6_4","Arm_D8_1","Arm_D8_2","Arm_D8_3","Arm_D8_4",
+                                  "Arm_D15_1","Arm_D15_2","Arm_D15_3","Arm_D15_4","Arm_D30_1","Arm_D30_2","Arm_D30_3","Arm_D30_4","cl13_D6_1","cl13_D6_2",
+                                  "cl13_D6_3","cl13_D6_4","cl13_D8_1","cl13_D8_2","cl13_D8_3","cl13_D8_4","cl13_D15_1","cl13_D15_2","cl13_D15_3","cl13_D15_4","cl13_D30_1",
+                                  "cl13_D30_2","cl13_D30_3","cl13_D30_4")
+
+Arm = Wherry_Exprs_Matrix[,1:20]
+cl13 = Wherry_Exprs_Matrix[,c(1:4,21:36)]
+
+boxplot(Arm)
+
+rm(Annotated,Wherry,Wherry_Exprs,Wherry_Exprs_Matrix)
+
+## Sarkar
+Sarkar = getGEO("GSE10239")
+Sarkar_Exprs = data.frame(exprs(Sarkar[[1]]))
+
+## Annotation
+x <- mouse4302SYMBOL
+mapped_probes <- mappedkeys(x)
+xx <- as.list(x[mapped_probes])
+Symbols = data.frame(Symbol = unlist(xx))
+Sarkar_Exprs = merge(Symbols,Sarkar_Exprs,by = 0)
+
+Sarkar_Arm = data.matrix(Sarkar_Exprs[,3:ncol(Sarkar_Exprs)])
+rownames(Sarkar_Arm) = toupper(Sarkar_Exprs$Symbol)
+colnames(Sarkar_Arm) = c("N_1","N_2","N_3","Memory_1","Memory_2",
+                         "Memory_3","KLRG1int_1","KLRG1int_2","KLRG1int_3",
+                         "KLRG1hi_1","KLRG1hi_2","KLRG1hi_3")
+
+Sarkar_Arm = Sarkar_Arm[,c(1:3,10:12,4:6)]
+Sarkar_Arm = log2(Sarkar_Arm+1)
+boxplot(Sarkar_Arm)
+
+rm(Sarkar_Exprs,Sarkar,x,xx,Symbols)
+
+## SV40-TAG
+GSE89307 <- read_delim("GSE89307_FPKM.txt","\t", escape_double = FALSE, trim_ws = TRUE)
+
+SV40 = data.matrix(GSE89307[,3:ncol(GSE89307)])
+SV40 = SV40[,-c(1:6,27:35)]
+rownames(SV40) = GSE89307$Gene_Symbol
+colnames(SV40) = sapply(strsplit(colnames(SV40),split = "_"),function(x) paste(x[-1],collapse = "_"))
+# Sheitinger_Matrix = Sheitinger_Matrix[rowSums(Sheitinger_Matrix)>0,]
+SV40 = log2((SV40+1))
+SV40 = SV40[rowSums(SV40)>0,]
+rownames(SV40) = toupper(rownames(SV40))
+SV40 = SV40[,rev(1:ncol(SV40))]
+
+rm(GSE89307,Sheitinger_Matrix,mapped_probes)
+
+## Kupper TRM
+Kupper = getGEO("GSE79805")
+Kupper_Exprs = data.frame(exprs(Kupper[[1]]))
+
+Annotated = data.frame(ACCNUM = sapply(contents(mogene20sttranscriptclusterACCNUM), paste, collapse = ","),SYMBOL = sapply(contents(mogene20sttranscriptclusterSYMBOL),paste,collapse = ","))
+Kupper_Exprs <- merge(Annotated, Kupper_Exprs,by=0,all=T)
+
+Kupper_VacV = data.matrix(Kupper_Exprs[,4:ncol(Kupper_Exprs)])
+rownames(Kupper_VacV) = toupper(Kupper_Exprs$SYMBOL)
+colnames(Kupper_VacV) = c("N", "CM", "EM", "d5", "d10", "d15", "d20", "d25", "d30", "d45", "d60", "d90", "OT-I Fasbp4/5 dKO skin infiltrating cells 10 days post infection", "OT-I Fasbp4/5 dKO skin infiltrating cells 30 days post infection")
+
+## Removes irrelevant timepoints
+Kupper_VacV = Kupper_VacV[,-c(ncol(Kupper_VacV),ncol(Kupper_VacV)-1)]
+Kupper_VacV = Kupper_VacV[,c(2,3,1,4:12)]
+
+boxplot(Kupper_VacV)
+rm(Kupper,Kupper_Exprs)
+
+KineticLists = list(Sarkar_Arm = Sarkar_Arm, 
+                    Arm = Arm, 
+                    cl13 = cl13, 
+                    SV40 = SV40,
+                    Kupper_VacV = Kupper_VacV)
+
+save(KineticLists,file = "KineticSets.rda",compression_level = 9)
